f76fa65e2cd44328a049d5ea4ad649cdc8da5c63
UdyneOS <51253393+nurfinzmuhamad14@users.noreply.github.com>
2025-08-27 09:53:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,72 +1,39 @@
 #!/bin/bash
 cat > /xray.json <<EOF
 {
-    "log": {
-        "loglevel": "warning"
-    },
-    "inbounds": [
-        {
-            "port": 443,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "550e8400-e29b-41d4-a716-446655440000", // 填写你的 UUID
-                        "level": 0,
-                        "email": "mnrdnn@admin.com"
-                    }
-                ],
-                "decryption": "none",
-                "fallbacks": [
-                    {
-                        "dest": 80
-                    },
-                    {
-                        "path": "/vless-ws",
-                        "dest": 1234,
-                        "xver": 1
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "tls",
-                "tlsSettings": {
-                    "alpn": [
-                        "http/1.1"
-                    ]                    
-                }
-            }
-        },
-        {
-            "port": 1234,
-            "listen": "127.0.0.1",
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "550e8400-e29b-41d4-a716-446655440000", // 填写你的 UUID
-                        "level": 0,
-                        "email": "mnrdnn@admin.com"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
-                    "path": "/vless-ws" // 必须换成自定义的 PATH，需要和上面的一致
-                }
-            }
+  "log": {
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "port": 443,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "550e8400-e29b-41d4-a716-446655440000",
+            "level": 0,
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "wsSettings": {
+          "path": "/vless",
+          "headers": {
+            "Host": "x-vless-freemium.up.railway.app"
+          }
         }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 EOF
 
